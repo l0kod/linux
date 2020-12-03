be94d4ba67b6098a10a66b7ab2d517aa384e9d78
Mickaël Salaün <mic@linux.microsoft.com>
2020-12-03 18:22:47 +0100

    arch: Wire up trusted_for(2)
    Wire up trusted_for(2) for all architectures.
    
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Kees Cook <keescook@chromium.org>
    Signed-off-by: Mickaël Salaün <mic@linux.microsoft.com>
    Reviewed-by: Thibaut Sautereau <thibaut.sautereau@ssi.gouv.fr>
    Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    ---
    
    Changes since v11:
    * Add Acked-by: Geert Uytterhoeven <geert@linux-m68k.org>
    * Rebase and leave space for watch_mount(2) and epoll_pwait2(2) from
      -next.
    
    Changes since v9:
    * Rename introspect_access(2) to trusted_for(2).
    * Increase syscall number to leave space for memfd_secret(2) in -next.
    
    Changes since v7:
    * New patch for the new syscall.
    * Increase syscall numbers by 2 to leave space for new ones (in
      linux-next): watch_mount(2) and process_madvise(2).

--- arch/arm64/include/asm/unistd32.h
+++ arch/arm64/include/asm/unistd32.h
@@ -889,6 +889,8 @@ __SYSCALL(__NR_pidfd_getfd, sys_pidfd_getfd)
 __SYSCALL(__NR_faccessat2, sys_faccessat2)
 #define __NR_process_madvise 440
 __SYSCALL(__NR_process_madvise, sys_process_madvise)
+#define __NR_trusted_for 443
+__SYSCALL(__NR_trusted_for, sys_trusted_for)
 
 /*
  * Please add new compat syscalls above this comment and update
